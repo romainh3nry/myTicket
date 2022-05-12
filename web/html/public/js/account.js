ff09156f1de1a6251fc577373b5c22fd16ce57f2
Romain HENRY <romainhenry60@gmail.com>
2022-05-12 14:34:49 +0200

    add spinner when searching users

--- web/html/public/js/account.js
+++ web/html/public/js/account.js
@@ -8,27 +8,48 @@ $(document).ready(function() {
     })
 });
 
+function displaySpinner() {
+    $('#search-users-results').append(
+        '<tr>\
+            <td class="text-center" colspan="4">\
+                <div class="spinner-grow text-warning" role="status"> \
+                    <span class="sr-only">Loading...</span> \
+                </div>\
+            </td>\
+        </tr>'
+    )
+}
+
 function findUsers(search) {
+    displaySpinner();
     $.ajax({
         url : '/api/users/' + search,
         method: 'get',
         dataType: 'json',
         success: function(data) {
+            $('#search-users-results').empty()
             data.forEach(element => {
                 $('#search-users-results').append(
-                    `<tr> \
-                    <td>${element.username}</td>\
-                    <td>${element.email}</td>\
-                    <td>${element.role}</td>\
-                    <td><button class="btn btn-sm"><a class="text-light" href="/users/update/${element.id}">Modifier</a></button></td>\
+                    `<tr>
+                        <td>${element.username}</td>
+                        <td>${element.email}</td>
+                        <td>${element.role}</td>
+                        <td>
+                            <button class="btn btn-sm">
+                                <a class="text-light" href="/users/update/${element.id}">Modifier</a>
+                            </button>
+                        </td>
                     </tr>`
                 )
             });
         },
         error: function(xhr, textStatus, error) {
+            $('#search-users-results').empty()
             $('#search-users-results').append(
                 `<tr> \
-                <td class="text-center" colspan="4"><div class="alert alert-danger">Erreur :  ${xhr.statusText}</div></td>\
+                    <td class="text-center" colspan="4">
+                        <div class="alert alert-danger">Erreur :  ${xhr.statusText}</div>
+                    </td>\
                 </tr>`
             )
         }
