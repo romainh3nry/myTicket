6b8ec8dc594cf40908150293d3ecf50ef390fc73
Romain HENRY <romainhenry60@gmail.com>
2022-05-21 11:17:10 +0200

    update table tickets - change id for uuid type and add column ticket_id

--- sql_scripts/tickets.sql
+++ sql_scripts/tickets.sql
@@ -1,5 +1,8 @@
+CREATE SEQUENCE tickets_id_seq;
+
 CREATE TABLE tickets (
-	id SERIAL NOT NULL PRIMARY KEY,
+	id uuid DEFAULT uuid_generate_v4(),
+    ticket_id VARCHAR(255) NOT NULL UNIQUE DEFAULT TO_CHAR(nextval('tickets_id_seq'), 'fm000000'),
     title VARCHAR(255) NOT NULL,
     date_creation TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     date_closure TIMESTAMPTZ,
@@ -8,6 +11,7 @@ CREATE TABLE tickets (
     state BOOLEAN NOT NULL DEFAULT TRUE,
     message TEXT NOT NULL,
     related_to uuid NOT NULL,
+    PRIMARY KEY (id),
 
     CONSTRAINT fk_author
         FOREIGN KEY(author)
@@ -21,3 +25,8 @@ CREATE TABLE tickets (
         FOREIGN KEY(related_to)
             REFERENCES customers(id)
 );
+
+ALTER SEQUENCE tickets_id_seq OWNED BY tickets.ticket_id;
+
+INSERT INTO tickets (title, author, service, message, related_to) VALUES
+    ('test title ticket', (SELECT id FROM users WHERE username = 'r.henry'), (SELECT id FROM services WHERE name = 'VOIP'), 'Cecis est un test de création de ticket !', (SELECT id FROM customers WHERE name = 'SFR'));
